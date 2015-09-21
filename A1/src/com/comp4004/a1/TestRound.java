01ef26578348c1bf6f1aa3f0087d6c924bd17766
Laura Alkhoury <alaura05@ca.ibm.com>
2015-09-21 16:36:37 -0400

    Add setUp method to TestRound to reduce code duplication

--- A1/src/com/comp4004/a1/TestRound.java
+++ A1/src/com/comp4004/a1/TestRound.java
@@ -4,27 +4,32 @@ package com.comp4004.a1;
 import junit.framework.*; 
 
 public class TestRound extends TestCase {
+	Round r;
+	
 	public TestRound(String name) {
 	  super(name);
 	}
 	
+	public void setUp(int num_players) {
+		r = Round.initRound(num_players);
+	}
+	
 	public void testSetNumPlayers() {
 		int num_players = 3;
-		Round r = Round.initRound(num_players);
+		setUp(num_players);
 		assertEquals(num_players, r.getNumPlayers());
 	}
 	
 	public void testLowerPlayerLimit() {
 		int num_players = 1;
-		Round r = Round.initRound(num_players);
-		
+		setUp(num_players);
 		// initRound should return null if num_players less than 2
 		assertEquals(null, r);
 	}
 	
 	public void testUpperPlayerLimit() {
 		int num_players = 5;
-		Round r = Round.initRound(num_players);
+		setUp(num_players);
 		
 		// initRound should return null if num_players greater than 4
 		assertEquals(null, r);
@@ -32,7 +37,7 @@ public class TestRound extends TestCase {
 	
 	public void testPlayerArrSize() {
 		int num_players = 4;
-		Round r = Round.initRound(num_players);
+		setUp(num_players);
 		
 		assertEquals(num_players, r.playerIDs.length);
 	}
